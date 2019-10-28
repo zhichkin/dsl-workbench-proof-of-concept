6fbd937815e13026fe807a460bba66949d9b5320
zhichkin <dima_zhichkin@mail.ru>
2019-10-28 03:06:25 +0300

    MetadataManager - parsing
    parsing DBNames is finished
    parsing DBSchema is almost finished

--- src/Metadata.Manager/MetadataManager.cs
+++ src/Metadata.Manager/MetadataManager.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.IO;
 using System.IO.Compression;
+using System.Collections.Generic;
 
 namespace OneCSharp.Metadata
 {
@@ -46,18 +47,14 @@ namespace OneCSharp.Metadata
             ReadIBVersion(connectionString, IBVersionPath);
             ReadConfig(connectionString, "root", RootPath); // + version + versions
             ReadConfig(connectionString, "e0666db2-45d6-49b4-a200-061c6ba7d569", ConfigPath);
+            
+            ReadConfig(connectionString, "db4a9ccb-9ef5-4b3c-8577-b6fe5db1b62e", Path.Combine(calalogPath, "db4a9ccb-9ef5-4b3c-8577-b6fe5db1b62e.txt"));
+            ReadConfig(connectionString, "30ee720f-b02b-4bab-8c66-bd27d8c2e8cb", Path.Combine(calalogPath, "30ee720f-b02b-4bab-8c66-bd27d8c2e8cb.txt"));
+            ReadConfig(connectionString, "cb3a5c5b-7bdc-4e12-96f1-11b1213b6853", Path.Combine(calalogPath, "cb3a5c5b-7bdc-4e12-96f1-11b1213b6853.txt"));
+            ReadConfig(connectionString, "9ad3b432-5b49-44ee-9d8d-83c36458d927", Path.Combine(calalogPath, "9ad3b432-5b49-44ee-9d8d-83c36458d927.txt"));
+            ReadConfig(connectionString, "c1568f1c-25ab-4328-8e77-e0e84788f10f", Path.Combine(calalogPath, "c1568f1c-25ab-4328-8e77-e0e84788f10f.txt"));
 
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.0", Path.Combine(calalogPath, "root-0.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.2", Path.Combine(calalogPath, "root-2.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.4", Path.Combine(calalogPath, "root-4.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.5", Path.Combine(calalogPath, "root-5.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.6", Path.Combine(calalogPath, "root-6.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.7", Path.Combine(calalogPath, "root-7.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.8", Path.Combine(calalogPath, "root-8.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.9", Path.Combine(calalogPath, "root-9.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.a", Path.Combine(calalogPath, "root-a.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.b", Path.Combine(calalogPath, "root-b.txt"));
-            ReadConfig(connectionString, "f0ba0954-a66b-4085-9df1-b8a4283bdbd3.c", Path.Combine(calalogPath, "root-c.txt"));
+            ReadCurrentSchema(connectionString, Path.Combine(calalogPath, "SchemaStorage.txt"));
         }
         public void ReadIBVersion(string connectionString, string filePath)
         {
@@ -266,8 +263,107 @@ namespace OneCSharp.Metadata
                 DecompressBinaryData(binaryData, filePath);
             }
         }
-    
-    
 
+
+        public void ReadCurrentSchema(string connectionString, string filePath)
+        {
+            SqlBytes[] binaryData = new SqlBytes[3];
+
+            { // limited scope for variables declared in it - using statement does like that - used here to get control over catch block
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = connection.CreateCommand();
+                SqlDataReader reader = null;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT CurrentSchema, NewGenCreated, NewGenDropped FROM SchemaStorage WHERE SchemaID = 0";
+                
+                try
+                {
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        binaryData[0] = reader.GetSqlBytes(0);
+                        binaryData[1] = reader.GetSqlBytes(1);
+                        binaryData[2] = reader.GetSqlBytes(2);
+                    }
+                }
+                catch (Exception error)
+                {
+                    WriteToLog(GetErrorText(error));
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        if (reader.HasRows) command.Cancel();
+                        reader.Dispose();
+                    }
+                    if (command != null) command.Dispose();
+                    if (connection != null) connection.Dispose();
+                }
+            } // end of limited scope
+
+            if (binaryData == null)
+            {
+                WriteToLog("SchemaStorage info is not found or empty.");
+            }
+            else
+            {
+                WriteBinaryDataToFile(binaryData[0], filePath);
+                WriteBinaryDataToFile(binaryData[1], filePath.Replace("SchemaStorage", "NewGenCreated"));
+                WriteBinaryDataToFile(binaryData[2], filePath.Replace("SchemaStorage", "NewGenDropped"));
+            }
+        }
+        public void ReadIBParamsInf(string connectionString, string filePath)
+        {
+            SqlBytes binaryData = null;
+
+            { // limited scope for variables declared in it - using statement does like that - used here to get control over catch block
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = connection.CreateCommand();
+                SqlDataReader reader = null;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT BinaryData FROM Params WHERE FileName = N'ibparams.inf' ORDER BY PartNo";
+
+                try
+                {
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        binaryData = reader.GetSqlBytes(0);
+                    }
+                }
+                catch (Exception error)
+                {
+                    WriteToLog(GetErrorText(error));
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        if (reader.HasRows) command.Cancel();
+                        reader.Dispose();
+                    }
+                    if (command != null) command.Dispose();
+                    if (connection != null) connection.Dispose();
+                }
+            } // end of limited scope
+
+            if (binaryData == null)
+            {
+                WriteToLog("SchemaStorage info is not found or empty.");
+            }
+            else
+            {
+                WriteBinaryDataToFile(binaryData, filePath);
+            }
+        }
+
+        public void ImportMetadata(string connectionString, string catalogPath)
+        {
+            DBName[] DBNames = QueryHelper.ReadDBNames(connectionString);
+            List<DBObject> DBObjects = QueryHelper.ReadDBSchema(connectionString, DBNames);
+        }
     }
 }
---
+++ src/Metadata.Manager/QueryHelper.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OneCSharp.Metadata
+{
+    internal sealed class DBName
+    {
+        internal string Token;
+        internal string FileName;
+    }
+    internal sealed class DBObject
+    {
+        internal int ID;
+        internal string Name;
+        internal string Token;
+        internal string FileName;
+        internal List<DBProperty> Properties = new List<DBProperty>();
+    }
+    internal sealed class DBProperty
+    {
+        internal int ID;
+        internal string Name;
+        internal List<string> Types = new List<string>();
+    }
+
+    internal static class QueryHelper
+    {
+        public static Stream ReadConfigFile(string connectionString, string fileName)
+        {
+            SqlBytes binaryData = null;
+
+            { // limited scope for variables declared in it - using statement does like that - used here to get control over catch block
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = connection.CreateCommand();
+                SqlDataReader reader = null;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT BinaryData FROM Config WHERE FileName = @FileName ORDER BY PartNo ASC";
+                command.Parameters.AddWithValue("FileName", fileName);
+                try
+                {
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        binaryData = reader.GetSqlBytes(0);
+                    }
+                }
+                catch (Exception error)
+                {
+                    // TODO: log error
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        if (reader.HasRows) command.Cancel();
+                        reader.Dispose();
+                    }
+                    if (command != null) command.Dispose();
+                    if (connection != null) connection.Dispose();
+                }
+            } // end of limited scope
+
+            return binaryData?.Stream;
+        }
+
+
+        public static DBName[] ReadDBNames(string connectionString)
+        {
+            SqlBytes binaryData = GetDBNamesFromDatabase(connectionString);
+            if (binaryData == null) return null;
+            DeflateStream stream = new DeflateStream(binaryData.Stream, CompressionMode.Decompress);
+            return ParseDBNames(stream);
+        }
+        private static SqlBytes GetDBNamesFromDatabase(string connectionString)
+        {
+            SqlBytes binaryData = null;
+
+            { // limited scope for variables declared in it - using statement does like that - used here to get control over catch block
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = connection.CreateCommand();
+                SqlDataReader reader = null;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT BinaryData FROM Params WHERE FileName = N'DBNames'";
+                try
+                {
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        binaryData = reader.GetSqlBytes(0);
+                    }
+                }
+                catch (Exception error)
+                {
+                    // TODO: log error
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        if (reader.HasRows) command.Cancel();
+                        reader.Dispose();
+                    }
+                    if (command != null) command.Dispose();
+                    if (connection != null) connection.Dispose();
+                }
+            } // end of limited scope
+
+            return binaryData;
+        }
+        private static DBName[] ParseDBNames(Stream stream)
+        {
+            DBName[] dbnames = null;
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line = reader.ReadLine();
+                if (line != null)
+                {
+                    int capacity = GetDBNamesCapacity(line);
+                    dbnames = new DBName[capacity + 1];
+                    
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        ParseDBNameLine(line, dbnames);
+                    }
+                }
+            }
+            return dbnames;
+        }
+        private static int GetDBNamesCapacity(string line)
+        {
+            return int.Parse(line.Replace("{", string.Empty).Replace(",", string.Empty));
+        }
+        private static void ParseDBNameLine(string line, DBName[] dbnames)
+        {
+            string[] items = line.Split(',');
+            if (items.Length < 3) return;
+
+            DBName dbname = new DBName()
+            {
+                FileName = items[0].Replace("{", string.Empty),
+                Token = items[1].Replace("\"", string.Empty)
+            };
+            int index = int.Parse(items[2].Replace("}", string.Empty));
+            dbnames[index] = dbname;
+        }
+
+
+        public static List<DBObject> ReadDBSchema(string connectionString, DBName[] dbnames)
+        {
+            SqlBytes binaryData = GetDBSchemaFromDatabase(connectionString);
+            if (binaryData == null) return null;
+            return ParseDBSchema(binaryData.Stream, dbnames);
+        }
+        public static SqlBytes GetDBSchemaFromDatabase(string connectionString)
+        {
+            SqlBytes binaryData = null;
+
+            { // limited scope for variables declared in it - using statement does like that - used here to get control over catch block
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = connection.CreateCommand();
+                SqlDataReader reader = null;
+                command.CommandType = CommandType.Text;
+                command.CommandText = "SELECT TOP 1 SerializedData FROM DBSchema";
+                try
+                {
+                    connection.Open();
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        binaryData = reader.GetSqlBytes(0);
+                    }
+                }
+                catch (Exception error)
+                {
+                    // TODO: log error
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        if (reader.HasRows) command.Cancel();
+                        reader.Dispose();
+                    }
+                    if (command != null) command.Dispose();
+                    if (connection != null) connection.Dispose();
+                }
+            } // end of limited scope
+
+            return binaryData;
+        }
+        private static List<DBObject> ParseDBSchema(Stream stream, DBName[] dbnames)
+        {
+            List<DBObject> result = new List<DBObject>();
+            using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ParseDBObjectLine(line, dbnames, result);
+                }
+            }
+            return result;
+        }
+        private static void ParseDBObjectLine(string line, DBName[] dbnames, List<DBObject> result)
+        {
+            Match match = Regex.Match(line, "^{\"\\w+\",\"N\",\\d+,\"\","); // Example: {"Reference42","N",42,"",
+            if (match.Success)
+            {
+                string[] items = line.Split(',');
+                DBObject dbo = new DBObject()
+                {
+                    Name = items[0].Replace("{", string.Empty).Replace("\"", string.Empty),
+                    ID = int.Parse(items[2])
+                };
+                dbo.Token = dbo.Name.Replace(dbo.ID.ToString(), string.Empty);
+                DBName dbname = dbnames[dbo.ID];
+                if (dbo.Token == dbname.Token)
+                {
+                    dbo.FileName = dbname.FileName;
+                }
+                result.Add(dbo);
+            }
+        }
+    }
+}
